473a233c7953a55ff5db1952e91676fa0ffa61f3
Matheus Dias <matheuscasmd@gmail.com>
2024-06-27 17:45:47 -0300

    criacao do projeto

---
+++ foodsharing-webapp/src/main/java/com/tasteful/foodsharing_webapp/TastefulApplication.java
@@ -0,0 +1,13 @@
+package com.tasteful.foodsharing_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastefulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastefulApplication.class, args);
+	}
+
+}
---
+++ foodsharing-webapp/src/test/java/com/tasteful/foodsharing_webapp/TastefulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasteful.foodsharing_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastefulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
